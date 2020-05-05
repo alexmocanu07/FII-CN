682cb61b2546afaf6f264e0101b88c9002c1c7e2
alexmocanu07 <46425351+alexmocanu07@users.noreply.github.com>
2020-05-05 10:42:53 +0300

    t5 - almost done

--- tema5.py
+++ tema5.py
@@ -1,5 +1,6 @@
 import random
 from Matrix import Matrix
+import numpy as np
 class prob:
     n = None
     p = None
@@ -71,6 +72,10 @@ class prob:
         return A
 
     @staticmethod
+    def generate_classic_matrix(linCount, colCount):
+        return [[round(random.uniform(-10, 10), 2) for _ in range(colCount)] for _ in range(linCount)]
+
+    @staticmethod
     def multiply_matrix_vector(matrix, vector):
         result = [0 for _ in range(prob.n)]
         for i in range(prob.n):
@@ -138,6 +143,90 @@ class prob:
             result = self.power_method(matrix)
         return result
 
+    @staticmethod
+    def get_singular_values(matrix):
+        return np.linalg.svd(matrix)
+
+    @staticmethod
+    def get_matrix_rank(singular_values):
+        rank = 0
+        for value in singular_values:
+            if value > 0:
+                rank += 1
+        return rank
+
+
+    @staticmethod
+    def get_matrix_condition(singular_values):
+        return max(singular_values) / min(singular_values)
+
+    @staticmethod
+    def compute_sI(singular_values, linCount, colCount):
+        # linCount = p si colCount = n
+        sI=[[0 for _ in range(linCount)] for _ in range(colCount)]
+        positive_values = [value for value in singular_values if value > 0]
+        for i in range(len(positive_values)):
+            sI[i][i] = 1.0 / positive_values[i]
+        print(sI)
+        return sI
+
+    @staticmethod
+    def multiply_matrixes(a, b):
+        result = [[0 for _ in range(len(b[0]))] for _ in range(len(a))]
+        for i in range(len(a)):
+            for j in range(len(b[0])):
+                for k in range(len(b)):
+                    result[i][j] += a[i][k] * b[k][j]
+        return result
+
+    @staticmethod
+    def get_matrix_MoorePenrose(matrix):
+        u, singular_values, v = prob.get_singular_values(matrix)
+        u = u.tolist()
+        singular_values = singular_values.tolist()
+        v = v.tolist()
+        sI=prob.compute_sI(singular_values, len(matrix), len(matrix[0]))
+        uT = np.matrix.transpose(np.array(u)).tolist()
+        return np.dot(np.dot(v, sI), uT)
+
+    @staticmethod
+    def multiply_classic_matrix_vector(matrix, vector):
+        result = [0 for _ in range(len(matrix))]
+        for i in range(len(matrix)):
+            sum = 0
+            for j in range(len(matrix[i])):
+                sum += vector[j] * matrix[i][j]
+            result[i] = sum
+        return result
+
+    @staticmethod
+    def solve_system(matrix, free):
+        return np.linalg.lstsq(matrix, free)
+
 if __name__ == '__main__':
     p = prob()
-    p.solve()
+    # p.solve()
+    linCount = 10
+    colCount = 7
+    matrix = prob.generate_classic_matrix(linCount, colCount)
+    free = [round(random.uniform(-10, 10), 2) for _ in range(linCount)]
+    singular_values = prob.get_singular_values(matrix)[1].tolist()
+    print(singular_values)
+    print(prob.get_matrix_rank(singular_values))
+    print(prob.get_matrix_condition(singular_values))
+    # print(np.linalg.cond(matrix))
+    # print(u)
+    # print(v)
+    pseudoInverse = prob.get_matrix_MoorePenrose(matrix)
+    print(pseudoInverse)
+    print(np.linalg.pinv(matrix))
+    # pinv = prob.get_matrix_MoorePenrose(matrix)
+    # print(pinv)
+    xI = prob.multiply_classic_matrix_vector(pseudoInverse, free)
+    print(xI)
+
+    print(prob.euclidean_norm(free, prob.multiply_classic_matrix_vector(matrix, xI)))
+    # solution = prob.solve_system(matrix, free)
+    # print(solution)
+    # print(prob.euclidean_norm(free, prob.multiply_classic_matrix_vector(matrix, solution)))
+
